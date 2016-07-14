4185340eb4d0ea13c61b3a2d553f5c61f2e48152
Alexandra <gangrel773@gmail.com>
2016-07-14 07:04:08 +0300

    add home work1

---
+++ HomeWork1/HomeWork1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type byte:\n default value {0}, min {1}, max {2}\n ", new Byte { }, Byte.MinValue, Byte.MaxValue);
+
+            Console.WriteLine("Type sbyte:\n default value {0}, min {1}, max {2}\n", new SByte { }, SByte.MinValue, SByte.MaxValue);
+
+            Console.WriteLine("Type short:\n default value {0}, min {1}, max {2}\n", new Int16 { }, Int16.MinValue, Int16.MaxValue);
+
+            Console.WriteLine("Type ushort:\n default value {0}, min {1}, max {2}\n", new UInt16 { }, UInt16.MinValue, UInt16.MaxValue);
+
+            Console.WriteLine("Type int:\n default value {0}, min {1}, max {2}\n", new Int32 { }, Int32.MinValue, Int32.MaxValue);
+
+            Console.WriteLine("Type uint:\n default value {0}, min {1}, max {2}\n", new UInt32 { }, UInt32.MinValue, UInt32.MaxValue);
+
+            Console.WriteLine("Type long:\n default value {0}, min {1}, max {2}\n", new Int64 { }, Int64.MinValue, Int64.MaxValue);
+
+            Console.WriteLine("Type ulong:\n default value {0}, min {1}, max {2}\n", new UInt64 { }, UInt64.MinValue, UInt64.MaxValue);
+
+            Console.WriteLine("Type float:\n default value {0}, min {1}, max {2}\n", new Single { }, Single.MinValue, Single.MaxValue);
+
+            Console.WriteLine("Type double:\n default value {0}, min {1}, max {2}\n", new Double { }, Double.MinValue, Double.MaxValue);
+
+            Console.WriteLine("Type decimal:\n default value {0}, min {1}, max {2}\n\n", new Decimal { }, Decimal.MinValue, Decimal.MaxValue);
+
+            Console.WriteLine("Type bool:\n default value {0}\n", new Boolean { });
+
+            Console.WriteLine("Type char:\n default value {0}\n", new Char { });
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWork1_1/HomeWork1_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string helloWorld = "Hello world";
+
+            for (int i = 0; i < helloWorld.Length; i++)
+            {
+                Console.WriteLine("Char {0}: dec {1}, hex {1:X}", helloWorld[i], (int)helloWorld[i]);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
