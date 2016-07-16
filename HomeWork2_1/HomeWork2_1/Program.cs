849e8e857b5f6f6821710d02767ca888a9037ec7
Alexandra <gangrel773@gmail.com>
2016-07-17 01:23:21 +0300

    add home work 2_1

---
+++ HomeWork2_1/HomeWork2_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace HomeWork2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1234567890;
+            string result="";
+            while (number > 0)
+            {
+                int n = number % 10;
+
+                if (n % 2 != 0 && n != 9)
+                {
+                    n++;
+                }
+                else if (n == 9)
+                {
+                    n = 0;
+                }
+
+                string str = n.ToString();
+                result = str + result;
+
+                number /= 10;
+            }
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
