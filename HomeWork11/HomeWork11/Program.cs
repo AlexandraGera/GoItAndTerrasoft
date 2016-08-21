6df55bed5e3e9c9e3ac8f56b3a408476a183ed47
Alexandra <gangrel773@gmail.com>
2016-08-21 03:39:50 +0300

    готово

--- HomeWork11/HomeWork11/Program.cs
+++ HomeWork11/HomeWork11/Program.cs
@@ -1,91 +1,108 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
 namespace HomeWork11
 {
     class Program
     {
-        delegate void WriteInformation<T>(T a);
+        delegate void WriteInformation();
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.GetEncoding(866);
+            WorkWhithInfo workWhithInfo = new WorkWhithInfo();
+            workWhithInfo.MainMenu();
+        }
 
-            while (true)
-            {
-                Console.Clear();
-                Console.WriteLine("Введите текст или число:");
-                string input = Console.ReadLine();
-                bool resultInspection = Inspection(input);
-                if (!resultInspection) continue;
-                Console.Write("\nПовторить?(1-Повторить/ Любая другая клавиша- закрыть) : ");
-                input = Console.ReadLine();
-                if (input == "1") continue;
-                else break;
-            }
-        }
-        static bool Inspection(string inputArg)
+        class WorkWhithInfo
         {
-            decimal input = 0;
-            if(Decimal.TryParse(inputArg, out input)){
-                input = Convert.ToDecimal(inputArg);
-                input *= input;
-                OutputSelection(input);
-               
-                return true;
-            }
-            else if (!String.IsNullOrEmpty(inputArg))
+            delegate void WriteInformation();
+            private string InputString { get; set; }
+            private decimal InputDecimal { get; set; }
+
+            public void MainMenu()
             {
-                OutputSelection(inputArg);
-                return true;
-            }
-            else
-            {
-                Console.WriteLine("Строка пуста!\nНажмите любую клавишу...");
-                return false;
-            }
-        }
-        static void OutputSelection<T>(T infoArg)
-        {
-        
-            while (true)
-            {
-                
-                Console.Clear();
-                Console.WriteLine("\tЧто сделать с информацией?\n1. Вывести на консоль\n2. Записать в файл");
-                string selection = Console.ReadLine();
-                if (selection == "1")
+                while (true)
                 {
-                    ShowInConsole(infoArg);
-                    break;
+                    Console.Clear();
+                    Console.WriteLine("Введите текст или число:");
+                    string input = Console.ReadLine();
+                    bool resultInspection = Inspection(input);
+                    if (!resultInspection) continue;
+                    OutputSelection();
+                    Console.Write("\nПовторить?(1-Повторить/ Любая другая клавиша- закрыть) : ");
+                    input = Console.ReadLine();
+                    if (input == "1") continue;
+                    else break;
                 }
-                else if (selection == "2")
+            }
+
+            private bool Inspection(string inputArg)
+            {
+                decimal input = 0;
+                if (Decimal.TryParse(inputArg, out input))
                 {
-                    WriteInFile(infoArg);
-                    break;
+                    InputDecimal = Convert.ToDecimal(inputArg);
+                    InputDecimal *= InputDecimal;
+                    return true;
+                }
+                else if (!String.IsNullOrEmpty(inputArg))
+                {
+                    InputString = inputArg;
+                    return true;
                 }
                 else
                 {
-                    Console.WriteLine("Ввод не верный\nНажмите любую клавишу...");
+                    Console.WriteLine("Строка пуста!\nНажмите любую клавишу...");
                     Console.ReadLine();
-                    continue;
+                    return false;
                 }
             }
-        }
-        static void ShowInConsole<T>(T infoArg)
-        {
-            Console.WriteLine("\n"+infoArg);
-        }
-        static void WriteInFile<T>(T infoArg)
-        {
-            using (StreamWriter writer = new StreamWriter("result.txt", true))
+
+            private void OutputSelection()
             {
-                writer.WriteLine(infoArg);
+                WriteInformation writeInfo;
+                while (true)
+                {
+                    Console.Clear();
+                    Console.WriteLine("\tЧто сделать с информацией?\n1. Вывести на консоль\n2. Записать в файл");
+                    string selection = Console.ReadLine();
+                    if (selection == "1")
+                    {
+                        if (InputString == default(string)) writeInfo = () => { Console.WriteLine("\n" + InputDecimal); InputDecimal = default(decimal); };
+                        else writeInfo = () => { Console.WriteLine("\n" + InputString); InputString = default(string); };
+                        break;
+                    }
+                    else if (selection == "2")
+                    {
+                        if (InputString == default(string)) writeInfo = () =>
+                        {
+                            using (StreamWriter writer = new StreamWriter("result.txt", true))
+                            {
+                                writer.WriteLine(InputDecimal);
+                                InputDecimal = default(decimal);
+                            }
+                            Console.WriteLine("\nЗапись сделана");
+                        };
+                        else writeInfo = () => {
+                            using (StreamWriter writer = new StreamWriter("result.txt", true))
+                            {
+                                writer.WriteLine(InputString);
+                                InputString = default(string);
+                            }
+                            Console.WriteLine("\nЗапись сделана");
+                        };
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ввод не верный\nНажмите любую клавишу...");
+                        Console.ReadLine();
+                        continue;
+                    }
+                }
+                writeInfo?.Invoke();
             }
-            Console.WriteLine("\nЗапись сделана");
         }
     }
 }
